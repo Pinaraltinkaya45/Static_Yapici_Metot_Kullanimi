3384d338355b063aeef17edca36a8db588754646
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-04 16:02:16 +0300

    Add project files.

---
+++ Static_Yapici_Metot_Kullanimi/Kullanici.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Yapici_Metot_Kullanimi
+{
+    public class Kullanici
+    {
+        private int kullaniciID { get; set; }
+        private string isim { get; set; }
+        private string soyisim { get; set; }
+        private static int maas { get; set; }
+
+
+        static Kullanici()
+        {
+            maas = 250000;
+        }
+        public Kullanici(int kullaniciid,string isim,string soyisim)
+        {
+            this.kullaniciID = kullaniciid;
+            this.isim = isim;
+            this.soyisim = soyisim;
+
+        }
+            
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("********* KULLANİCİ BİLGİLERİ **********");
+            Console.WriteLine("İd : " + kullaniciID);
+            Console.WriteLine("İsim : " + isim);
+            Console.WriteLine("Soyisim : " + soyisim);
+            Console.WriteLine("Maas : " + maas);
+        }
+
+        public void zamYap(int zamMiktari)
+        {
+            Console.WriteLine("Kullaniciya Zam Yapılıyor..");
+            Console.WriteLine("Maaşı : " + maas);
+            Console.WriteLine("Yeni Maaşı : " + (maas + zamMiktari));
+
+        }
+
+    }
+}
---
+++ Static_Yapici_Metot_Kullanimi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Yapici_Metot_Kullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kullanici k1 = new Kullanici(1, "Pınar", "Altınkaya");
+
+            k1.bilgileriGoster();
+            k1.zamYap(5000);
+         
+            Console.ReadLine();
+        }
+    }
+}
